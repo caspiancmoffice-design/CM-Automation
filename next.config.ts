61afa63f08f1be9fb72d44f954e8577bc368fa41
caspiancmoffice-design <caspiancmoffice@gmail.com>
2026-09-17 13:43:50 +0330

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? "/cm-automation" : "",
+  assetPrefix: isProd ? "/cm-automation/" : "",
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: false,
+};
+
+export default nextConfig;
